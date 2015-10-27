9f8f964a0c3a058fccf681f14e0350741dbc71c4
Luis Fernando Oliveira <luismilanooliveira@gmail.com>
2015-10-27 19:25:33 -0200

    done encode and decode

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,10 +1,31 @@
 (ns alphabet-cipher.coder)
 
+(defn char-to-int [x]
+  (- (int x) (int \a)))
+
+(defn int-to-char [x]
+  (char (+ x (int \a))))
+
+(defn normalize [x]
+  (if (< x (char-to-int \a))
+    (+ x (char-to-int \z) 1)
+        (if (> x (char-to-int \z))
+            (- x (char-to-int \z) 1)
+            x)))
+
 (defn encode [keyword message]
-  "encodeme")
+  (let [keyword-keys (map char-to-int keyword)
+        message-keys (map char-to-int message)
+        summed-keys  (map + message-keys (cycle keyword-keys))
+        normalized-keys (map normalize summed-keys)]
+    (apply str (map int-to-char normalized-keys))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (let [keyword-keys (map char-to-int keyword)
+        message-keys (map char-to-int message)
+        diffed-keys  (map - message-keys (cycle keyword-keys))
+        normalized-keys (map normalize diffed-keys)]
+  (apply str (map int-to-char normalized-keys))))
 
 (defn decipher [cipher message]
   "decypherme")
